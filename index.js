9c1914d934947a6c99947613add006b1b33210a5
Sadat Ahmad <s.ahmad-1@student.utwente.nl>
2022-07-16 00:41:10 +0530

    Init

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require('axios').default
+require('dotenv').config()
+
+const client_id = process.env.CLIENT_ID
+const client_secret = process.env.client_secret
+const base_url = "https://fiu-uat.setu.co"
+
