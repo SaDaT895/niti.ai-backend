a446156ee52acdb55c48f7f53c09f8737759e2b5
Sadat Ahmad <s.ahmad-1@student.utwente.nl>
2022-07-17 01:19:57 +0530

    Setu webview connected

--- index.js
+++ index.js
@@ -5,30 +5,46 @@ const requestBody = require('./body')
 
 //Setting up env variables
 const client_id = process.env.CLIENT_ID
-const client_secret = process.env.client_secret
+const client_secret = process.env.CLIENT_SECRET
 const base_url = "https://fiu-uat.setu.co"
+const notif_url = process.env.NOTIF_URL
 
-//Setting up Axios headers
+//Global Axios headers
 axios.defaults.headers.common['x-client-id'] = client_id
 axios.defaults.headers.common['x-client-secret'] = client_secret
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 const app = express()
+let id;
 
 app.get('/', (req,res) => {
     //TODO: Front-end
     res.send('Listening')
 })
 
+//Request functions and helper functions
 const createNewConsent  = (mobileNumber) => {
     let bodyData = requestBody.consentDetails(mobileNumber)
     let response = axios.post(`${base_url}/consents`,bodyData)
     return response
 }
 
+//To get consent status directly
+const getConsentStatus = (id) => {
+    return axios.get(`${base_url}/consents/${id}`)
+}
+
+//Create Consent Request and Redirect to consent URL
 app.get('/createConsent/:mobileNumber', async (req,res) => {
     let consentRes  = await createNewConsent(req.params.mobileNumber)
-    res.redirect(consentRes.data.url)
+    id = consentRes.data.id
+    res.send(`Approve consent at ${consentRes.data.url}`)
+})
+
+//Await Consent status update
+app.post(notif_url, async (req,res) => {
+    console.log(req.body.data)
+    res.sendStatus(200)
 })
 
 app.listen(3000, () => console.log('Up and Running at http://localhost:3000'))
