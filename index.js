118e96c808c67cb4cc9ac092217307fdd1dcce9f
Sadat Ahmad <s.ahmad-1@student.utwente.nl>
2022-07-16 14:48:50 +0530

    express integration

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
+const express = require('express')
 const axios = require('axios')
 require('dotenv').config()
 const requestBody = require('./body')
 
+//Setting up env variables
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.client_secret
 const base_url = "https://fiu-uat.setu.co"
 
+//Setting up Axios headers
 axios.defaults.headers.common['x-client-id'] = client_id
 axios.defaults.headers.common['x-client-secret'] = client_secret
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-const createNewConsent  = async (mobileNumber) => {
+const app = express()
+
+app.get('/', (req,res) => {
+    //TODO: Front-end
+    res.send('Listening')
+})
+
+const createNewConsent  = (mobileNumber) => {
     let bodyData = requestBody.consentDetails(mobileNumber)
-    axios.post(`${base_url}/consents`,bodyData)
-    .then(res => console.log(res)).catch(err => console.log(err))
+    let response = axios.post(`${base_url}/consents`,bodyData)
+    return response
 }
 
-createNewConsent('9999999999')
+app.get('/createConsent/:mobileNumber', async (req,res) => {
+    let consentRes  = await createNewConsent(req.params.mobileNumber)
+    res.redirect(consentRes.data.url)
+})
+
+app.listen(3000, () => console.log('Up and Running at http://localhost:3000'))
